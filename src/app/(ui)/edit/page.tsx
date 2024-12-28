d93372b24fa44785d30e71e3b3addb2af1e915ef
Victor-Spichenkoff <victorspich13@gmail.com>
2024-12-28 20:16:08 -0300

    Remove extra page

--- src/app/(ui)/edit/page.tsx
+++ src/app/(ui)/edit/page.tsx
@@ -15,7 +15,6 @@ import {ShowMessage} from "@/components/utils/Message";
 import {api} from "@/lib/api";
 import {TextArea} from "@/components/ui/TextArea";
 import {useCheckLogin} from "@/hooks/useCheckLogin";
-import {RealPage} from "@/app/(ui)/edit/real_page";
 
 
 const giveCorrectCoverImage = (coverImageUrl: string) => {
@@ -27,165 +26,164 @@ const giveCorrectCoverImage = (coverImageUrl: string) => {
 
 
 export default function EditPage() {
-    if(typeof location == "undefined" || typeof window == "undefined" || typeof  localStorage =="undefined")
+    useCheckLogin()
+
+    const params = useParams()
+    const {toast} = useToast()
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const [name, setName] = useState("")
+    const [bio, setBio] = useState("")
+    const [cover, setCover] = useState("")
+    const [avatar, setAvatar] = useState("")
+    const [link, setLink] = useState("")
+    const [showSaveButton, setShowSaveButton] = useState(false)
+    const [isLoaded, setIsLoaded] = useState(false)// erro vercel
+
+
+    useEffect(() => {
+        if (typeof window === "undefined") return
+
+        if (getAuthData()?.user.slug != searchParams.get("slug")) {
+            router.push("/edit/ladrao")
+            return
+        }
+
+        // erro vercel
+        if (
+            !searchParams ||
+            !searchParams.get("name") ||
+            !searchParams.get("bio") ||
+            !searchParams.get("link") ||
+            !searchParams.get("avatar") ||
+            !searchParams.get("cover")
+        ) {
+            router.push("/edit/error");
+            return;
+        }
+
+        setName(decodeURIComponent(String(searchParams.get("name"))))
+        setLink(decodeURIComponent(String(searchParams.get("link"))))
+        setBio(decodeURIComponent(String(searchParams.get("bio"))))
+        setAvatar(decodeURIComponent(String(searchParams.get("avatar"))))
+        setCover(decodeURIComponent(String(searchParams.get("cover"))))
+
+        setIsLoaded(true);
+    }, [])
+
+    //verificar mudanças
+    useEffect(() => {
+        if (!searchParams) return
+
+        const url_name = decodeURIComponent(String(searchParams.get("name")))
+        const url_link = decodeURIComponent(String(searchParams.get("link")))
+        const url_bio = decodeURIComponent(String(searchParams.get("bio")))
+
+        if (url_name != name || url_link != link || url_bio != bio)
+            setShowSaveButton(true)
+        else
+            setShowSaveButton(false)
+    }, [name, link, bio])
+
+
+    const handleSaveChanges = async () => {
+        try {
+            const res = await api.put("/user", {
+                name,
+                bio,
+                link
+            })
+
+            if (res.data) {
+                ShowMessage(
+                    toast,
+                    "Salvo com sucesso!",
+                    true
+                )
+
+                setShowSaveButton(false)
+            }
+
+        } catch {
+            ShowMessage(
+                toast,
+                "Não foi possível salvar as alterações"
+            )
+        }
+    }
+
+
+    if (!isLoaded)
         return null
 
-    return <RealPage />
+    return (
+        <div>
+            <GeneralHeader backHref="/home">
+                <div className="font-bold text-lg">Editar perfil</div>
+            </GeneralHeader>
+            <section className="border-b-2 border-gray-900 pb-10">
+                {/* Contêiner de imagem de capa */}
+                <div
+                    style={{position: 'relative', width: '100%'}}
+                    className="bg-gray-500 h-32 bg-no-repeat bg-cover bg-center z-0"
+                >
+                    <div className="flex justify-center items-center">
+                        <Image
+                            src={giveCorrectCoverImage(String(cover)) ?? ""}
+                            fill
+                            alt="cover"
+                            style={{objectFit: 'cover', zIndex: -10}}
+                        />
 
-    // useCheckLogin()
-    //
-    // const params = useParams()
-    // const {toast} = useToast()
-    // const router = useRouter()
-    // const searchParams = useSearchParams()
-    // const [name, setName] = useState("")
-    // const [bio, setBio] = useState("")
-    // const [cover, setCover] = useState("")
-    // const [avatar, setAvatar] = useState("")
-    // const [link, setLink] = useState("")
-    // const [showSaveButton, setShowSaveButton] = useState(false)
-    // const [isLoaded, setIsLoaded] = useState(false)// erro vercel
-    //
-    //
-    // useEffect(() => {
-    //     if (typeof window === "undefined") return
-    //
-    //     // if (getAuthData()?.user.slug != searchParams.get("slug")) {
-    //     //     router.push("/edit/ladrao")
-    //     //     return
-    //     // }
-    //
-    //     // erro vercel
-    //     if (
-    //         !searchParams ||
-    //         !searchParams.get("name") ||
-    //         !searchParams.get("bio") ||
-    //         !searchParams.get("link") ||
-    //         !searchParams.get("avatar") ||
-    //         !searchParams.get("cover")
-    //     ) {
-    //         router.push("/edit/error");
-    //         return;
-    //     }
-    //
-    //     setName(decodeURIComponent(String(searchParams.get("name"))))
-    //     setLink(decodeURIComponent(String(searchParams.get("link"))))
-    //     setBio(decodeURIComponent(String(searchParams.get("bio"))))
-    //     setAvatar(decodeURIComponent(String(searchParams.get("avatar"))))
-    //     setCover(decodeURIComponent(String(searchParams.get("cover"))))
-    //
-    //     setIsLoaded(true);
-    // }, [searchParams, router])
-    //
-    // //verificar mudanças
-    // useEffect(() => {
-    //     if (!searchParams) return
-    //
-    //     const url_name = decodeURIComponent(String(searchParams.get("name")))
-    //     const url_link = decodeURIComponent(String(searchParams.get("link")))
-    //     const url_bio = decodeURIComponent(String(searchParams.get("bio")))
-    //
-    //     if (url_name != name || url_link != link || url_bio != bio)
-    //         setShowSaveButton(true)
-    //     else
-    //         setShowSaveButton(false)
-    // }, [name, link, bio])
-    //
-    //
-    // const handleSaveChanges = async () => {
-    //     try {
-    //         const res = await api.put("/user", {
-    //             name,
-    //             bio,
-    //             link
-    //         })
-    //
-    //         if (res.data) {
-    //             ShowMessage(
-    //                 toast,
-    //                 "Salvo com sucesso!",
-    //                 true
-    //             )
-    //
-    //             setShowSaveButton(false)
-    //         }
-    //
-    //     } catch {
-    //         ShowMessage(
-    //             toast,
-    //             "Não foi possível salvar as alterações"
-    //         )
-    //     }
-    // }
-    //
-    // return (
-    //     <div>
-    //         <GeneralHeader backHref="/home">
-    //             <div className="font-bold text-lg">Editar perfil</div>
-    //         </GeneralHeader>
-    //         <section className="border-b-2 border-gray-900 pb-10">
-    //             {/* Contêiner de imagem de capa */}
-    //             <div
-    //                 style={{position: 'relative', width: '100%'}}
-    //                 className="bg-gray-500 h-32 bg-no-repeat bg-cover bg-center z-0"
-    //             >
-    //                 <div className="flex justify-center items-center">
-    //                     <Image
-    //                         src={giveCorrectCoverImage(String(cover)) ?? ""}
-    //                         fill
-    //                         alt="cover"
-    //                         style={{objectFit: 'cover', zIndex: -10}}
-    //                     />
-    //
-    //                     <div className="absolute inset-0 flex justify-center items-center space-x-4">
-    //                         <div
-    //                             className={"cursor-pointer rounded-full size-12 bg-black/85 hover:bg-gray-800/85 flex items-center justify-center"}>
-    //                             <FontAwesomeIcon icon={faCamera} className="size-6 text-white"/>
-    //                         </div>
-    //                         <div
-    //                             className={"cursor-pointer rounded-full size-12 bg-black/85 hover:bg-gray-800/85 flex items-center justify-center"}>
-    //                             <FontAwesomeIcon icon={faXmark} className="size-6 text-white"/>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //
-    //             {/* Contêiner do usuário */}
-    //             <div className="flex justify-between items-end">
-    //                 <div className="relative flex justify-between items-end -mt-12 z-10">
-    //                     {/* Avatar do usuário */}
-    //                     <Image
-    //                         src={avatar ?? ""}
-    //                         alt="avatar"
-    //                         className="rounded-full "
-    //                         height={96}
-    //                         width={96}
-    //                     />
-    //
-    //                     <div className="absolute inset-0 flex justify-center items-center space-x-4">
-    //                         <div
-    //                             className={"cursor-pointer rounded-full size-6 bg-black/90 hover:bg-gray-800/90 flex items-center justify-center"}>
-    //                             <FontAwesomeIcon icon={faCamera} className="size-3 text-white"/>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //
-    //             </div>
-    //
-    //             {/*Infos editáveis user */}
-    //             <div className="px-6 mt-4 space-y-4">
-    //                 <Input placeholder={"Nome"} setState={setName} value={name} label={"Nome"}/>
-    //                 <Input placeholder={"Link"} setState={setLink} value={link} label={"Link"}/>
-    //                 <TextArea placeholder={"Descreva você"} rows={4} value={bio} setState={setBio} label={"Bio"} />
-    //             </div>
-    //             {showSaveButton && (
-    //                 <div className='flex justify-center items-center'>
-    //                     <MyButton label={"Salvar alterações"} size={"m"} onClick={handleSaveChanges}
-    //                               className={"p-2 mt-10"}/>
-    //
-    //                 </div>
-    //             )}
-    //         </section>
-    //     </div>
-    // )
+                        <div className="absolute inset-0 flex justify-center items-center space-x-4">
+                            <div
+                                className={"cursor-pointer rounded-full size-12 bg-black/85 hover:bg-gray-800/85 flex items-center justify-center"}>
+                                <FontAwesomeIcon icon={faCamera} className="size-6 text-white"/>
+                            </div>
+                            <div
+                                className={"cursor-pointer rounded-full size-12 bg-black/85 hover:bg-gray-800/85 flex items-center justify-center"}>
+                                <FontAwesomeIcon icon={faXmark} className="size-6 text-white"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Contêiner do usuário */}
+                <div className="flex justify-between items-end">
+                    <div className="relative flex justify-between items-end -mt-12 z-10">
+                        {/* Avatar do usuário */}
+                        <Image
+                            src={avatar ?? ""}
+                            alt="avatar"
+                            className="rounded-full "
+                            height={96}
+                            width={96}
+                        />
+
+                        <div className="absolute inset-0 flex justify-center items-center space-x-4">
+                            <div
+                                className={"cursor-pointer rounded-full size-6 bg-black/90 hover:bg-gray-800/90 flex items-center justify-center"}>
+                                <FontAwesomeIcon icon={faCamera} className="size-3 text-white"/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                {/*Infos editáveis user */}
+                <div className="px-6 mt-4 space-y-4">
+                    <Input placeholder={"Nome"} setState={setName} value={name} label={"Nome"}/>
+                    <Input placeholder={"Link"} setState={setLink} value={link} label={"Link"}/>
+                    <TextArea placeholder={"Descreva você"} rows={4} value={bio} setState={setBio} label={"Bio"}/>
+                </div>
+                {showSaveButton && (
+                    <div className='flex justify-center items-center'>
+                        <MyButton label={"Salvar alterações"} size={"m"} onClick={handleSaveChanges}
+                                  className={"p-2 mt-10"}/>
+
+                    </div>
+                )}
+            </section>
+        </div>
+    )
 }
